c216e78c09e66aea9245b5a58b389cb2cc879399
Vitalii Balabanov <tertiumnon@gmail.com>
2023-09-16 20:34:38 +0200

    add markdown parsing

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import express from 'express';
 import fs from 'fs';
 import { getFiles } from './utils/file.util';
+import MarkdownIt from 'markdown-it';
+
 const app = express();
+const md = new MarkdownIt(
+  {
+    html: true,
+    linkify: true,
+    typographer: true
+  }
+);
+
 const APP_PORT = 8080; // Default port to listen
 const TEST_DIR = 'test';
 
@@ -13,13 +23,13 @@ app.set('view engine', 'html');
 
 // Define a route handler for the default home page
 app.get('/', (req, res) => {
-  res.send('Hello world!');
+  res.send('NodeJS Markdown Wiki');
 });
 
 app.get('/test', (req, res) => {
   const path = __dirname + `/../${TEST_DIR}/test.md`;
   const file = fs.readFileSync(path, 'utf8');
-  res.send(file.toString());
+  res.send(md.render(file));
 });
 
 app.get('/files', (req, res) => {
